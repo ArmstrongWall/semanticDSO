e2eee65f8975714951d55c726f7651181d35e558
ArmstrongWall <160111223wzq@gmail.com>
2018-06-22 23:19:12 +0800

    add start cmd

--- start.sh
+++ start.sh
@@ -3,3 +3,7 @@
 		calib=~/git/dataset/kitti/data_odometry_gray/dataset/sequences/00/camera.txt \
         preset=0 \
         mode=1
+
+live
+cmake-build-debug/bin/dso_live files=/home/wzq/git/dataset/kitti/data_odometry_gray/dataset/sequences/00 calib=thirdparty/mycalib.txt preset=0 mode=1
+
